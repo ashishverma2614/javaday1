2a2afa87ff4b4de49cdc8a598f02935ad35955b3
ashish kumar verma <ashishverma2614@users.noreply.github.com>
2019-10-18 19:23:22 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,29 +1,35 @@
-public class PrimeExample
-	{  
-	 public static void main(String args[])
-		 {  
-			int i,m=0,flag=0;    
-			int n=3;//it is the number to be checked  
-			m=n/2;    
-			if(n==0||n==1)
-				  {
-					System.out.println(n+" is not prime number");    
-				  }
-			else
-				  {
-				   for(i=2;i<=m;i++)
-				   {    
-					if(n%i==0)
-						{    
-							System.out.println(n+" is not prime number");    
-							flag=1;    
-							break;    
-						}    
-				   }    
-			   if(flag==0) 
-				{ 
-					System.out.println(n+" is prime number"); 
-				}
-			}
-		}  
-	} 
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a Range to find prime number ");
+        System.out.println("Enter lower range value :");
+        int num1 = input.nextInt();
+        System.out.println("Enter higher range value :");
+        int num2 = input.nextInt();
+        
+        boolean flag = false;
+        for(int j = num1 ; j <=num2; j++)
+        {
+             flag = false;
+            for(int i = 2; i <= j/2; ++i)
+            {
+                // condition for nonprime number
+                if(j % i == 0)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag == false )
+                System.out.println( j + " is a prime number.");
+            else
+            {
+                //System.out.println( j + " is not a prime number.");
+            }
+        }
+    }
+}
